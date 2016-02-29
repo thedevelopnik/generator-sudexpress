f27ff4ba66ede0c9dac9ba483136659c722b670d
David Sudia <dsudia@gmail.com>
2016-02-29 11:13:48 -0700

    added dotenv package to aid in multiple deployments

--- app/templates/src/server/routes/index.js
+++ app/templates/src/server/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var escape = require('pg-escape');
+require('dotenv').config();
+connectionstring = process.env.DB_URL;
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
