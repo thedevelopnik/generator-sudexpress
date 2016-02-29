fca8e853361e6f8d032c45ec8fb8412d04370ff4
David Sudia <dsudia@gmail.com>
2016-02-29 16:28:41 -0700

    added some missing requires

--- app/templates/src/server/app.js
+++ app/templates/src/server/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var Promise = require('bluebird');
 
 
 // *** routes *** //
--- app/templates/src/server/routes/index.js
+++ app/templates/src/server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var escape = require('pg-escape');
+var pgp = require('pg-promise');
 require('dotenv').config();
 connectionstring = process.env.DB_URL;
 
