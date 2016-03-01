4e5fda81c7fdca71d5c1606c322879dff51e0129
David Sudia <dsudia@gmail.com>
2016-02-29 17:09:00 -0700

    fixed pathing for one file

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ var SudexpressGenerator = yeoman.Base.extend({
 
   createApp: function(){
     this.copy('./src/client/js/main.js', './src/client/js/main.js');
-    this.copy('./src/client/js/bundle.js', '/src/client/js/bundle.js');
+    this.copy('./src/client/js/bundle.js', './src/client/js/bundle.js');
     this.copy('./src/client/css/main.css', './src/client/css/main.css');
     this.copy('./src/server/bin/www', './src/server/bin/www');
     this.copy('./src/server/routes/index.js', './src/server/routes/index.js');
