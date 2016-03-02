a9b10e2d7feaafb0d1bb9440752494026f710b7b
David Sudia <dsudia@gmail.com>
2016-03-01 19:37:43 -0700

    now copies over knex files on generation

--- app/index.js
+++ app/index.js
@@ -24,6 +24,8 @@ var SudexpressGenerator = yeoman.Base.extend({
     this.copy('_gitignore', '.gitignore');
     this.copy('gulpfile.js', 'gulpfile.js');
     this.copy('./test/tests.spec.js', './test/tests.spec.js');
+    this.copy('./src/server/knex.js', './src/server/knex.js');
+    this.copy('./src/server/knexfile.js', './src/server/knexfile.js');
   },
 
 });
